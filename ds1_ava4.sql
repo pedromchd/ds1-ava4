729c15df8d24f7e0d13e2ddceaeb86fa06accc61
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-08-19 11:40:11 +0000

    Database config

--- ds1_ava4.sql
+++ ds1_ava4.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 14-Ago-2023 às 10:36
--- Versão do servidor: 8.0.33-0ubuntu0.22.04.2
--- versão do PHP: 7.3.33-8+ubuntu22.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Generation Time: Aug 19, 2023 at 01:37 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,41 +18,41 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `ds1_ava4`
+-- Database: `ds1_ava4`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `empregados`
+-- Table structure for table `empregados`
 --
 
 CREATE TABLE `empregados` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `salario_bruto` decimal(10,2) NOT NULL,
   `departamento` enum('1','2','3','4') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `empregados`
+-- Indexes for table `empregados`
 --
 ALTER TABLE `empregados`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `empregados`
+-- AUTO_INCREMENT for table `empregados`
 --
 ALTER TABLE `empregados`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
